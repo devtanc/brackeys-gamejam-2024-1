ffccfec398127ae6c287c6e6938366c6a7f4991d
Tanner <devtanc@gmail.com>
2024-02-13 22:22:54 -0700

    Add tilemap with layers (#2)
    * feat: add Room and Map classes to generate a randomized game map with different room types and connections
    
    * feat: add support for one-way connections between rooms and update room generation logic

--- room.gd
+++ room.gd
@@ -3,28 +3,32 @@ extends Node
 
 class RoomConnection:
 	var room: Room = null
+	# Should be 1 for 1-way or 2 for 2-way
 	var direction: int = 0
+	func _init(room_to_connect: Room, conn_direction: int):
+		assert(conn_direction == 1 || conn_direction == 2)
+		room = room_to_connect
+		direction = conn_direction
 
-enum RoomType {
-	None,
-	Start,
-	End,
-	Good,
-	Bad,
-	Gold,
-	Store,
-	VS,
-	Shadow
-}
+# Room types
+# "none",
+# "start",
+# "end",
+# "good",
+# "bad",
+# "gold",
+# "store",
+# "vs",
+# "shadow"
 
 var max_connection_count: int = 0
-var type: RoomType = RoomType.None
+var type: String = "none"
 var connected_rooms: Array[RoomConnection] = []
-# 1,2,3,4,5
+
 func _init():
 	max_connection_count = get_init_max_conn_count()
 	type = get_init_room_type()
-	pass
+
 	
 func get_init_max_conn_count():
 	var num = randf()
@@ -38,19 +42,22 @@ func get_init_max_conn_count():
 		return 4
 	else:
 		return 5
-	pass
 
-func get_init_room_type():
+func get_init_room_type() -> String:
 	var num = randf()
 	if num < 0.3:
-		return RoomType.None
+		return "none"
 	elif num < 0.6:
 		if randf() < 0.6:
-			return RoomType.Good
+			return "good"
 		else:
-			return RoomType.Bad
+			return "bad"
 	elif num < 0.8:
-		return RoomType.Gold
+		return "gold"
 	else:
-		return RoomType.Store
-	pass
+		return "store"
+	
+func add_connection(room: Room, direction: int) -> int:
+	connected_rooms.append(RoomConnection.new(room, direction))
+	return max_connection_count - connected_rooms.size()
+
